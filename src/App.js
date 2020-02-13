c9f309f81437048632a0ec766ab40c33743cc655
Mayu Imanaka <imamayu.1215@gmail.com>
2020-02-12 18:21:55 -0800

    add:fontawesome in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(fab, fas, far);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td width="50">
+                <FontAwesomeIcon className="titleIcon" icon={['fa', 'film']} />
+              </td>
+              <td width="8" />
+              <td>
+                <h1>Movies Search</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    );
+  }
 }
 
 export default App;
