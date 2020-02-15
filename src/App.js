62ce675b2d7885ae3136d76132a13457ee45e8d2
Mayu Imanaka <imamayu.1215@gmail.com>
2020-02-15 10:51:57 -0800

    add:input box,props,state for movie

--- src/App.js
+++ src/App.js
@@ -1,32 +1,94 @@
 import React, { Component } from 'react';
+import './App.scss';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 library.add(fab, fas, far);
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log("This is my initializer")
+
+    const movies = [
+      { id: 0, title: "Charlie's Angels", overview: "Three women, detectives with a mysterious boss, retrieve stolen voice-ID software, using martial arts, tech skills, and sex appeal." },
+      { id: 1, title: "Charlie's Angels: Full Throttle", overview: "The Angels are charged with finding a pair of missing rings that are encoded with the personal information of members of the Witness Protection Program. As informants are killed, the ladies target a rogue agent…" },
+    ]
+
+    this.state = {
+      rows: [
+        <p key="1">This is my row0</p>,
+        <p key="2">This is my row1</p>,
+        <p key="3">This is my row2</p>
+      ]
+    }
+
+    let movieRows = []
+    movies.forEach((movie) => {
+      console.log(movie.title)
+      const movieRow = <table>
+        <tbody>
+          <tr>
+            <td>
+              <img src="" alt="poster" />
+            </td>
+            <td>
+              {movie.title}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      movieRows.push(movieRow)
+    })
+
+    this.state = { movieRows: movieRows }
+  }
+
+
+
   render() {
     return (
       <div className="App">
-
-        <table className="titleBar">
+        <table className="titleBar"
+          style={{
+            backgroundColor: '#000',
+            display: 'block',
+            color: '#fff',
+            paddingLeft: 16
+          }}>
           <tbody>
             <tr>
               <td width="50">
-                <FontAwesomeIcon className="titleIcon" icon={['fa', 'film']} />
+                <FontAwesomeIcon
+                  className="titleIcon"
+                  icon={['fa', 'film']} />
               </td>
               <td width="8" />
               <td>
-                <h1>Movies Search</h1>
+                <h1>Movie Search</h1>
               </td>
             </tr>
           </tbody>
         </table>
 
-      </div>
+        <input
+          style={{
+            fontSize: 24,
+            display: 'block',
+            width: "97%",
+            paddingTop: 8,
+            paddingBottom: 8,
+            paddingLeft: 16
+          }}
+          placeholder="Enter Search Term" />
+
+        {this.state.rows}
+
+      </div >
+
+
     );
   }
 }
